442a8ca88b5f75128b95da741d97f0e2eeaa75eb
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2023-04-10 16:22:01 +0600

    learning

---
+++ dom/app.js
@@ -0,0 +1,23 @@
+//let id1 = document.getElementById("id-1"); // get id form html
+
+// id1.innerHTML = "Hello World!";
+
+// let className = document.getElementsByClassName("myclass")[1];
+
+// className.innerHTML = "Hello JavaScript";
+
+// let getName = document.getElementsByName("myName")[0];
+
+// getName.innerHTML = "Hello Nayeem Khan";
+
+// let getTag = document.getElementsByTagName("p")[1];
+
+// getTag.innerHTML = "hello JS";
+
+let queryClass = document.querySelector(".myclass");
+let queryId = document.querySelector("#id-1");
+let queryTag = document.querySelector("p");
+
+let queryAll = document.querySelectorAll("p")[1];
+
+queryAll.innerHTML = "Hello World!";
